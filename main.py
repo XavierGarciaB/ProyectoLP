5bb5e9da0a62033eb8213d23e561a1f1c3178c3c
Adriana Riofrio <alriofri@espol.edu.ec>
2021-06-20 17:14:11 -0500

    6 palabras reservadas, 15 símbolos y 5 caracteres especiales, y funciones para variables y constantes.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-# ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
-# ------------------------------------------------------------
 import ply.lex as lex
 
 # List of token names.   This is always required
+
+reserved = {
+    #Adriana R
+    'if': 'IF',
+    'else': 'ELSE',
+    'else if': "ELSEIF",
+    'end': 'END',
+    'false': 'FALSE',
+    'nil': 'NIL',
+    'for': 'FOR',
+    #Adriana R
+
+}
+
 tokens = (
     'NUMBER',
     'PLUS',
@@ -15,7 +23,32 @@ tokens = (
     'DIVIDE',
     'LPAREN',
     'RPAREN',
-)
+    #Adriana Riofrio
+    'DOT',
+    'NOTEQUAL',
+    'GREATERTHAN',
+    'LESSTHAN',
+    'EQUAL',
+    'GREQUAL',
+    'LEQUAL',
+    'COMBCOMP',
+    'PLUSASSIGN',
+    'MINASSIGN',
+    'MODASSIGN',
+    'EXPASSIGN',
+    'TIMESASSIGN',
+    'DIVASSIGN',
+    'RANGEINCLUSIVE',
+    'RANGEXCLUSIVE',
+    'CONSTANT',
+    'VAR',
+    'HORZTAB',
+    'VERTAB',
+    'SINGLEQUOTE',
+    'DOUBLEQUOTE',
+    #Adriana Riofrio
+
+) + tuple(reserved.values())
 
 # Regular expression rules for simple tokens
 t_PLUS = r'\+'
@@ -25,6 +58,40 @@ t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 
+#Adriana Riofrio
+t_DOT = r'\.'
+t_NOTEQUAL = r'!='
+t_GREATERTHAN = r'>'
+t_LESSTHAN = r'<'
+t_EQUAL = r'='
+t_GREQUAL = r'>='
+t_LEQUAL= r'<='
+t_COMBCOMP = r'<=>'
+t_PLUSASSIGN = r'\+='
+t_MINASSIGN = r'-='
+t_MODASSIGN = r'%='
+t_EXPASSIGN = r'\*\*='
+t_TIMESASSIGN = r'\*='
+t_DIVASSIGN = r'\\='
+t_RANGEINCLUSIVE = r'\.\.'
+t_RANGEXCLUSIVE = r'\.\.\.'
+t_HORZTAB = r'\\t'
+t_VERTAB = r'\\v'
+t_SINGLEQUOTE = r"\'"
+t_DOUBLEQUOTE = r'\\"'
+#Adriana Riofrio
+
+#Adriana Riofrio
+def t_VAR(t):
+    r'(@{0,2}|$)[a-z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'VAR')  # Check for reserved words
+    return t
+
+def t_CONSTANT(t):
+    r'[A-Z]+'
+    t.type = reserved.get(t.value, 'CONSTANT')
+    return t
+#Adriana Riofrio
 
 # A regular expression rule with some action code
 def t_NUMBER(t):
@@ -42,28 +109,22 @@ def t_newline(t):
 # A string containing ignored characters (spaces and tabs)
 t_ignore = ' \t'
 
-
 # Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-
+def getTokens(lexer):
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        print(tok)
 # Build the lexer
 lexer = lex.lex()
+linea=" "
+while linea!="":
+    linea=input(">>")
+    lexer.input(linea)
+    getTokens(lexer)
 
-# Test it out
-data = '''
- 3 + 4 * 10
-   + -20 *2
- '''
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # No more input
-    print(tok)
